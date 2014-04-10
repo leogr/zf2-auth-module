12422febaf2c10a55fb629b537e05867e3a7c179
visa4 <antonino.visalli@gmail.com>
2014-04-10 18:33:42 +0200

    modify adapter message

--- src/AuthModule/Adapter/ModelAdapter.php
+++ src/AuthModule/Adapter/ModelAdapter.php
@@ -93,23 +93,35 @@ class ModelAdapter implements AdapterInterface, ValidatableAdapterInterface
         $count = 0;
         foreach ($results as $identityObject) {
             if ($count > 1) {
-                return new Result(Result::FAILURE_IDENTITY_AMBIGUOUS, $identity);
+                return new Result(Result::FAILURE_IDENTITY_AMBIGUOUS,
+                    $identity,
+                    array('More than one record matches the supplied identity.')
+                );
             }
             $count++;
         }
 
         if ($count == 0) {
-            return new Result(Result::FAILURE_IDENTITY_NOT_FOUND, $identity);
+            return new Result(Result::FAILURE_IDENTITY_NOT_FOUND,
+                $identity,
+                array('A record with the supplied identity could not be found.')
+            );
         }
 
         if ($identityObject instanceof ObjectInterface) {
             if ($identityObject->validateCredential($this->getCredential())) {
                 return new Result(Result::SUCCESS, $identity);
             }//else
-            return new Result(Result::FAILURE, $identity);
+            return new Result(Result::FAILURE_CREDENTIAL_INVALID,
+                $identity,
+                array('wrong password')
+            );
         }
 
-        return new Result(Result::FAILURE_UNCATEGORIZED, $identity);
+        return new Result(Result::FAILURE_UNCATEGORIZED,
+            $identity,
+            array('generic error')
+        );
     }
 
 }
